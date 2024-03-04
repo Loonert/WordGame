4f6e94bc3031f577ab2628b4819f4c877f365082
user <user@DESKTOP-215B6PU>
2024-03-04 20:01:07 +0700

    another commit

--- WordsGame/words_game/urls.py
+++ WordsGame/words_game/urls.py
@@ -2,16 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='MainPage'),
-    # path('api/games/<int:game_id>/start', views.start_game, name='start_game'),
+    path('', views.MainPage, name='MainPage'),
     path('api/games/start', views.start_game, name='start_game'),
-
     path('game/<int:game_id>/', views.game_page, name='game_page'),
-
-    path('api/games/<int:game_id>/start', views.create_player, name='create_player'),
-
-    # path('api/games/start', views.start_game, name='start_game'),
-    path('api/games/<int:game_id>/', views.game_page, name='game_page'),
+    path('api/games/start/', views.create_player, name='create_player'),  # Add a trailing slash
     path('api/games/<int:game_id>/check-word', views.check_word, name='check_word'),
     path('api/games/<int:game_id>/pass-turn', views.pass_turn, name='pass_turn'),
     path('api/games/<int:game_id>/time-is-up', views.time_is_up, name='time_is_up'),
